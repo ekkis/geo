beefce63d1eabc43f83f407ed807e07a2ee17a89
Hermes Scanner <scan@hermes.local>
2026-05-04 14:36:01 +0000

    Transform country-list-js into a service using remote-lib-proxy

--- server.js
+++ server.js
@@ -1,35 +1,10 @@
-const country = require('./index');
+import { createServer } from 'remote-lib-proxy/server';
 
-module.exports = (req) => {
-    var cmd = url(req.url);
-    var fn = country[cmd.method];
-    var p = params(fn).map(nm => cmd[nm])
-    return fn.apply(country, p);
-}
+await createServer({
+  library: '.',
+  port: 3000,
+  name: 'Country List API',
+  description: 'ISO codes, capitals, currencies, dialing codes'
+}).start();
 
-function url(s) {
-    var ret = {}; 
-    var [, args] = s.split('?');
-    if (!args) return ret;
-
-    var r = args.split(/[&=]/);
-    for (var i = 0; i < r.length; i += 2) {
-        ret[r[i]] = r[i+1];
-    }   
-    return ret;
-}
-
-function params(f) {
-    var s = f.toString();
-    var re = [
-        /function\s+\w+\((.*?)\)/,
-        /function\s*\((.*?)\)/,
-        /\((.*?)\)\s*=>/,
-        /(\w+)\s*=>/
-    ]
-    for (var i = 0; i < re.length; i++) {
-        var r = s.match(re[i]);
-        if (r) return r[1].split(',');
-    }
-    return [];
-}
+console.log('Country API service running on port 3000');
